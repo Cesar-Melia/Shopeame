005c5411be0050a3b6883d2531e4d6761fa55204
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-05-26 15:16:44 +0200

    Products page base functions

--- products.js
+++ products.js
@@ -6,19 +6,56 @@ async function api(url) {
 }
 
 const divProducts$$ = document.body.querySelector('[data-function="products"]');
-console.log(divProducts$$);
+const galleryH2$$ = document.body.querySelector('[data-function="gallery-h2"]');
+const list$$ = document.body.querySelector('[data-function="button-list"]');
+const block$$ = document.body.querySelector('[data-function="button-block"]');
+const search$$ = document.body.querySelector('[data-function="search"]');
+
+let galleryCounter = 0;
 
 const printProducts = (products) => {
     for (product of products) {
-        const newDiv$$ = document.createElement("div");
-        newDiv$$.classList.add("col-6", "col-md-3", "b-gallery__product");
-        newDiv$$.innerHTML = `
-        <img src=${product.image} class="b-gallery__img">
-        <h4 class="b-gallery__name">${product.name}</h4>
-        <span class="b-galery__price">${product.price}</span>
-        <p class="b-gallery__desc">${product.description}</p>`;
+        const div$$ = document.createElement("div");
+        div$$.classList.add(
+            "b-gallery__product",
+            "d-flex",
+            "justify-content-end",
+            "col-12",
+            "col-sm-6",
+            "col-lg-3"
+        );
+        div$$.setAttribute("id", product.id);
+        div$$.innerHTML = `
+        <div class="b-gallery__img">
+            <img src=${product.image}>
+        </div>`;
 
-        divProducts$$.appendChild(newDiv$$);
+        const divText$$ = document.createElement("div");
+        divText$$.classList.add("b-gallery__text-block");
+        divText$$.innerHTML = `
+        <div classs="d-flex justify-content-end">
+            <h4 class="b-gallery__name">${product.name}</h4>
+            <span class="b-gallery__price">${product.price} €</span>
+            <p class="b-gallery__desc">${product.description}</p>
+        </div>`;
+
+        const divStars$$ = document.createElement("div");
+        divStars$$.classList.add("b-gallery__stars-block", "d-flex", "justify-content-between");
+        divStars$$.innerHTML = `
+        <div class="b-gallery__stars d-flex justify-content-between flex-wrap">${setStars(
+            product.stars
+        )}</div>`;
+
+        const button$$ = document.createElement("button");
+        button$$.classList.add("b-gallery__button-edit");
+        button$$.textContent = "Editar";
+        button$$.addEventListener("click", editProduct);
+
+        divStars$$.appendChild(button$$);
+        div$$.appendChild(divText$$);
+        div$$.appendChild(divStars$$);
+        divProducts$$.appendChild(div$$);
+        updateGalleryCounter();
     }
 };
 
@@ -26,3 +63,34 @@ api(baseUrl).then((products) => {
     console.log(products);
     printProducts(products);
 });
+
+const updateGalleryCounter = () => {
+    galleryCounter++;
+    galleryH2$$.textContent = `Listado de productos (${galleryCounter})`;
+};
+
+const setStars = (rating) => {
+    let htmlStr = "";
+    const fullStar = `<span class="b-icon__star icon-star-full"></span>`;
+    const halfStar = `<span class="b-icon__star icon-star-half"></span>`;
+    const emptyStar = `<span class="b-icon__star icon-star-empty"></span>`;
+
+    for (let i = 1; i <= 5; i++) {
+        if (Math.floor(rating) >= i) {
+            htmlStr += fullStar;
+        } else {
+            if ((rating % 1).toFixed(2) >= 0.5 && Math.floor(rating) >= i - 1) {
+                htmlStr += halfStar;
+            } else {
+                htmlStr += emptyStar;
+            }
+        }
+    }
+
+    htmlStr += `<span class="b-icon__star-text">&nbsp${rating}<span>`;
+    return htmlStr;
+};
+
+const editProduct = () => {
+    console.log("Editar un producto");
+};
