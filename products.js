20a91961262e8c95047ab03d20bb6cbbca925f50
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-05-27 12:07:17 +0200

    Reduced the number of API requests

--- products.js
+++ products.js
@@ -1,4 +1,6 @@
 const baseUrl = "http://localhost:3000/products";
+let products = [];
+let productsMod = [];
 
 async function api(url) {
     const res = await fetch(url);
@@ -18,17 +20,23 @@ const image$$ = document.body.querySelector('[data-function="image"]');
 
 let galleryCounter = 0;
 
-const printProducts = (products) => {
+const printProducts = (products = productsMod) => {
+    divProducts$$.innerHTML = "";
     for (product of products) {
         const div$$ = document.createElement("div");
-        div$$.classList.add(
-            "b-gallery__product",
-            "d-flex",
-            "justify-content-end",
-            "col-12",
-            "col-sm-6",
-            "col-lg-3"
-        );
+        if (window.location.href.includes("products.html")) {
+            div$$.classList.add(
+                "b-gallery__product",
+                "d-flex",
+                "justify-content-end",
+                "col-12",
+                "col-sm-6",
+                "col-lg-3"
+            );
+        }
+        if (window.location.href.includes("manage.html")) {
+            div$$.classList.add("col-12", "col-md-4");
+        }
         div$$.setAttribute("id", product.id);
         div$$.innerHTML = `
         <div class="b-gallery__img">
@@ -45,7 +53,12 @@ const printProducts = (products) => {
         </div>`;
 
         const divStars$$ = document.createElement("div");
-        divStars$$.classList.add("b-gallery__stars-block", "d-flex", "justify-content-between");
+        divStars$$.classList.add(
+            "b-gallery__stars-block",
+            "d-flex",
+            "justify-content-between",
+            "flex-wrap"
+        );
         divStars$$.innerHTML = `
         <div class="b-gallery__stars d-flex justify-content-between flex-wrap">${setStars(
             product.stars
@@ -55,7 +68,9 @@ const printProducts = (products) => {
         button$$.classList.add("b-gallery__button-edit");
         button$$.setAttribute("id", product.id);
         button$$.textContent = "Editar";
-        button$$.addEventListener("click", editProduct);
+        if (window.location.href.includes("products.html")) {
+            button$$.addEventListener("click", editProduct);
+        }
 
         divStars$$.appendChild(button$$);
         div$$.appendChild(divText$$);
@@ -65,28 +80,27 @@ const printProducts = (products) => {
     }
 };
 
-if (window.location.href.includes("products.html")) {
-    api(baseUrl).then((products) => {
-        printProducts(products);
-    });
-}
-
 const searchProducts = (event) => {
-    api(baseUrl).then((products) => {
-        divProducts$$.innerHTML = "";
-        galleryCounter = 0;
-        let productsList = products.filter((product) =>
+    if (event.target.value) {
+        productsMod = products.filter((product) =>
             product.name.toLowerCase().includes(event.target.value.toLowerCase())
         );
-        printProducts(productsList);
-    });
+    } else {
+        productsMod = products;
+    }
+    galleryCounter = 0;
+    printProducts();
 };
 
-search$$.addEventListener("input", searchProducts);
+if (window.location.href.includes("products.html")) {
+    search$$.addEventListener("input", searchProducts);
+}
 
 const updateGalleryCounter = () => {
     galleryCounter++;
-    galleryH2$$.textContent = `Listado de productos (${galleryCounter})`;
+    if (window.location.href.includes("products.html")) {
+        galleryH2$$.textContent = `Listado de productos (${galleryCounter})`;
+    }
 };
 
 const setStars = (rating) => {
@@ -109,6 +123,51 @@ const setStars = (rating) => {
 
     htmlStr += `<span class="b-icon__star-text">&nbsp${rating}<span>`;
     return htmlStr;
+};
+
+if (window.location.href.includes("management.html")) {
+    let numId = 0;
+    let preview = [
+        {
+            id: numId,
+            name: "Nombre",
+            price: 0,
+            description: "Descripción",
+            stars: 0,
+            image: "assets/images/img-default.png",
+        },
+    ];
+    printProducts(preview);
+
+    name$$.addEventListener("input", () => {
+        generatePreview(preview);
+    });
+    price$$.addEventListener("input", () => {
+        generatePreview(preview);
+    });
+    description$$.addEventListener("input", () => {
+        generatePreview(preview);
+    });
+    stars$$.addEventListener("input", () => {
+        generatePreview(preview);
+    });
+    image$$.addEventListener("input", () => {
+        generatePreview(preview);
+    });
+}
+
+const generatePreview = (preview) => {
+    numId = productsMod.length;
+    while (productsMod.find((product) => product.id === numId)) {
+        numId++;
+    }
+    preview[0].id = numId;
+    preview[0].name = name$$.value;
+    preview[0].price = price$$.value;
+    preview[0].description = description$$.value;
+    preview[0].stars = stars$$.value;
+    preview[0].image = image$$.value;
+    printProducts(preview);
 };
 
 //////////////////////////////  EDIT PRODUCT  ////////////////////////////
@@ -134,6 +193,12 @@ const editProduct = () => {
 //     fillInputs();
 // }
 
-// let preview = [
-//     { id: galleryCounter + 1, name: "", price: 0, description: "", stars: 0, image: "" },
-// ];
+window.onload = () => {
+    api(baseUrl).then((prods) => {
+        productsMod = prods;
+        products = prods;
+        if (window.location.href.includes("products.html")) {
+            printProducts();
+        }
+    });
+};
